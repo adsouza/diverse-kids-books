b7aa1795e16d254d827cf528be46fca99800facf
Tony D'souza <adsouza@gmail.com>
2021-07-10 09:09:56 -0400

    Fetch & display creators with their books.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/adsouza/diverse-kids-books
 
 go 1.15
 
-require cloud.google.com/go/firestore v1.5.0
+require (
+	cloud.google.com/go/firestore v1.5.0
+	google.golang.org/api v0.40.0
+)
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
         "log"
         "net/http"
         "os"
+	"strings"
 
+	"google.golang.org/api/iterator"
 	"cloud.google.com/go/firestore"
 )
 
@@ -13,6 +16,31 @@ func main() {
 	ctx := context.Background()
 	fsc := createClient(ctx)
 	defer fsc.Close()
+	var c Creator
+	iter := fsc.Collection("creators").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		doc.DataTo(&c)
+		books, err := c.FetchBooks(ctx)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		if len(books) == 0 {
+			continue
+		}
+		var titles []string
+		for _, b := range books {
+			titles = append(titles, b.Title)
+		}
+		fmt.Printf("%s: %s.\n", c.Name, strings.Join(titles, ", "))
+	}
+
 	port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"
@@ -22,6 +50,29 @@ func main() {
         if err := http.ListenAndServe(":"+port, nil); err != nil {
                 log.Fatal(err)
         }
+}
+
+type Book struct {
+	Title string
+}
+
+type Creator struct {
+	Name string
+	Books []*firestore.DocumentRef
+}
+
+func (c *Creator) FetchBooks(ctx context.Context) ([]*Book, error) {
+	var books []*Book
+	for _, r := range c.Books {
+		d, err := r.Get(ctx)
+		if err != nil {
+			return nil, err
+		}
+		var b Book
+		d.DataTo(&b)
+		books = append(books, &b)
+	}
+	return books, nil
 }
 
 func createClient(ctx context.Context) *firestore.Client {
