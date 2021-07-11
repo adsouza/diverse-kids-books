aab1f089f6480659b742eb12b9864b85fe72a861
Tony D'souza <adsouza@gmail.com>
2021-07-11 12:21:01 -0400

    Create an import cmd to upload data from a CSV to FireStore.

--- cmd/import.go
+++ cmd/import.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"io"
         "log"
-        "net/http"
         "os"
 	"strings"
 
@@ -16,6 +17,45 @@ func main() {
 	ctx := context.Background()
 	fsc := createClient(ctx)
 	defer fsc.Close()
+
+	// Read data from CSV & write it to FireStore.
+	f, err := os.Open("/tmp/diverse-kids-books.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	for {
+		row, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Skip books that lack illustrators.
+		if row[2] == "" {
+			continue
+		}
+		i := Creator{Name: row[2]}
+		iRef := fsc.Collection("creators").Doc(i.Name)
+		if _, err := iRef.Get(ctx); err != nil {
+			if _, err := iRef.Set(ctx, i); err != nil {
+				log.Fatal(err)
+			}
+		}
+		b := Book{
+			Title: row[0],
+			Illustrator: iRef,
+		}
+		if _, err := fsc.Collection("books").Doc(fmt.Sprintf("%s by %s", b.Title, row[1])).Set(ctx, b); err != nil {
+			log.Fatal(err)
+		}
+		log.Println(row[0])
+	}
+	fmt.Println("--")
+
+	// Read data from FireStore & print it to console.
 	var (
 		c Creator
 		b Book
@@ -45,17 +85,6 @@ func main() {
 		}
 		fmt.Printf("%s: %s.\n", c.Name, strings.Join(titles, ", "))
 	}
-
-	// Start server.
-	port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-                log.Printf("Defaulting to port %s", port)
-        }
-        log.Printf("Listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
-                log.Fatal(err)
-        }
 }
 
 type Book struct {
