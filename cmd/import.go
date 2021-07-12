1978a09764681c1241dd8396e1bec1bee3f25f64
Tony D'souza <adsouza@gmail.com>
2021-07-12 16:54:16 -0400

    Handle creator pairs who share a last name.

--- cmd/import.go
+++ cmd/import.go
@@ -50,9 +50,26 @@ func main() {
 func importCreators(ctx context.Context, fsc *firestore.Client, names string) []*firestore.DocumentRef {
 	var refs []*firestore.DocumentRef
 	creators := strings.Split(names, " &\n")
-	for _, n := range creators {
-		i := Creator{Name: n}
-		ref := fsc.Collection("creators").Doc(n)
+	var people []string
+	for _, c := range creators {
+		if !strings.Contains(c, " & ") {
+			people  = append(people, c)
+			continue
+		}
+		// Handle pairs of creators (typically family) who share a last name.
+		couple := strings.Split(c, " & ")
+		if len(couple) != 2 {
+			log.Fatalf("Only 2 people allowed per couple: %s", c)
+		}
+		pieces := strings.Split(couple[1], " ")
+		// Does not account for unhypenated multi-word family names (e.g. von or del)
+		last := pieces[len(pieces)-1]
+		people = append(people, fmt.Sprintf("%s %s", couple[0], last))
+		people = append(people, couple[1])
+	}
+	for _, p := range people {
+		i := Creator{Name: p}
+		ref := fsc.Collection("creators").Doc(p)
 		if _, err := ref.Get(ctx); err != nil {
 			if _, err := ref.Set(ctx, i); err != nil {
 				log.Fatal(err)
